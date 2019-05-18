f6a54e6877d5a401056089b6c9883c0b4877d2cd
jaeyoung.choi <lactrious@gmail.com>
2019-05-18 15:47:16 +0900

    Rewrite response type of server stub

--- client/src/server.ts
+++ client/src/server.ts
@@ -14,14 +14,14 @@ type ApiResponse<T> = {
 };
 
 type GifItems = { data: GifItem[] };
-export const fetchRandom50 = (): Promise<ApiResponse<GifItem[]>> =>
+export const fetchRandom50 = (): Promise<ApiResponse<GifItems>> =>
   fetch(`${baseUrl}/random50`).then(r => r.json());
 
 export const getItem = (id: string): Promise<ApiResponse<GifItem>> =>
   fetch(`${baseUrl}/${id}`).then(r => r.json());
 
-export const listAll = (page: number): Promise<ApiResponse<GifItem[]>> =>
+export const listAll = (page: number): Promise<ApiResponse<GifItems>> =>
   fetch(`${baseUrl}/all`).then(r => r.json());
 
-export const search = (q: string): Promise<ApiResponse<GifItem[]>> =>
+export const search = (q: string): Promise<ApiResponse<GifItems>> =>
   fetch(`${baseUrl}/search?q=` + encodeURIComponent(q)).then(r => r.json());
