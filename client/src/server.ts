83eaf6a59c24a0e7ec1f408b43a289e9bda25626
jaeyoung.choi <lactrious@gmail.com>
2019-05-18 15:46:49 +0900

    Add server stub

---
+++ client/src/server.ts
@@ -0,0 +1,27 @@
+type GifItem = {
+  kind: string;
+  type: string;
+  id: string;
+  slug: string;
+  url: string;
+  createdAt: string;
+};
+
+const baseUrl = "https://gdg-webtech-hackathon-backend.firebaseapp.com/api/gif";
+
+type ApiResponse<T> = {
+  data: T;
+};
+
+type GifItems = { data: GifItem[] };
+export const fetchRandom50 = (): Promise<ApiResponse<GifItem[]>> =>
+  fetch(`${baseUrl}/random50`).then(r => r.json());
+
+export const getItem = (id: string): Promise<ApiResponse<GifItem>> =>
+  fetch(`${baseUrl}/${id}`).then(r => r.json());
+
+export const listAll = (page: number): Promise<ApiResponse<GifItem[]>> =>
+  fetch(`${baseUrl}/all`).then(r => r.json());
+
+export const search = (q: string): Promise<ApiResponse<GifItem[]>> =>
+  fetch(`${baseUrl}/search?q=` + encodeURIComponent(q)).then(r => r.json());
